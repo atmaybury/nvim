eb30e8f8af228eebb61044e03c750e04b9f960e0
Andrew Maybury <andrewmaybury@Andrews-MacBook-Pro.local>
2024-06-10 23:01:56 +1000

    Moved setting theme back to init.lua

--- init.lua
+++ init.lua
@@ -1,1 +1,4 @@
 require("andrew")
+
+-- set colorscheme
+vim.cmd [[colorscheme tokyonight]]
--- lua/andrew/set.lua
+++ lua/andrew/set.lua
@@ -2,6 +2,3 @@ vim.opt.termguicolors = true
 
 vim.opt.number = true
 vim.opt.relativenumber = true
-
--- set colorscheme
-vim.cmd [[colorscheme tokyonight]]
