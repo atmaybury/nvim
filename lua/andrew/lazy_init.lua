6aeac066ae1e2b3e8b8eb7207f0491cc0136c30d
Andrew M <andrewm@pixeltechnologies.com.au>
2024-06-12 09:51:09 +1000

    Fixed lsp keybindings

--- lua/andrew/lazy_init.lua
+++ lua/andrew/lazy_init.lua
@@ -1,33 +1,70 @@
 -- initialize lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-	vim.fn.system({
-		"git",
-		"clone",
-		"--filter=blob:none",
-		"https://github.com/folke/lazy.nvim.git",
-		"--branch=stable", -- latest stable release
-		lazypath,
-	})
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
 end
 vim.opt.rtp:prepend(lazypath)
 
 -- load plugin definitions
 require("lazy").setup({
-	spec = "andrew.lazy",
-	change_detection = { notify = false }
+  spec = "andrew.lazy",
+  change_detection = { notify = false }
 })
 
 -- setup lsp-zero
 local lsp_zero = require('lsp-zero')
 lsp_zero.on_attach(function(client, bufnr)
-	lsp_zero.default_keymaps({ buffer = bufnr })
+  lsp_zero.default_keymaps({ buffer = bufnr })
 end)
 
 -- setup mason for lsp-zero downloads
 require('mason').setup({})
 require('mason-lspconfig').setup({
-	handlers = {
-		lsp_zero.default_setup,
-	},
+  handlers = {
+    lsp_zero.default_setup,
+  },
+})
+
+local cmp = require('cmp')
+cmp.setup({
+  preselect = 'item',
+  completion = {
+    completeopt = 'menu,menuone,noinsert'
+  },
+  sources = {
+    { name = 'nvim_lsp' },
+  },
+  mapping = {
+    ['<CR>'] = cmp.mapping.confirm({ select = false }),
+    ['<C-c>'] = cmp.mapping.abort(),
+    ['<Up>'] = cmp.mapping.select_prev_item({ behavior = 'select' }),
+    ['<Down>'] = cmp.mapping.select_next_item({ behavior = 'select' }),
+    ['<Tab>'] = cmp.mapping.select_next_item({ behavior = 'select' }),
+    -- ['<C-p>'] = cmp.mapping(function()
+    --   if cmp.visible() then
+    --     cmp.select_prev_item({behavior = 'insert'})
+    --   else
+    --     cmp.complete()
+    --   end
+    -- end),
+    -- ['<C-n>'] = cmp.mapping(function()
+    --   if cmp.visible() then
+    --     cmp.select_next_item({behavior = 'insert'})
+    --   else
+    --     cmp.complete()
+    --   end
+    -- end),
+  },
+  snippet = {
+    expand = function(args)
+      require('luasnip').lsp_expand(args.body)
+    end,
+  },
 })
