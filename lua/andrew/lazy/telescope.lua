ede35c26b3564a5b778a322f27d7ee75c7efb5cf
Andrew Maybury <andrew.maybury@leaveplus.com.au>
2025-11-11 12:26:46 +1100

    Added <leader>tl keybind to toggle telescope layout

--- lua/andrew/lazy/telescope.lua
+++ lua/andrew/lazy/telescope.lua
@@ -2,10 +2,25 @@ return {
   "nvim-telescope/telescope.nvim",
   dependencies = { "nvim-lua/plenary.nvim" },
   config = function()
-    require("telescope").setup({
+    local telescope = require("telescope")
+
+    local layout = "vertical"
+
+    telescope.setup({
       defaults = {
-        layout_strategy = "vertical"
-      }
+        layout_strategy = layout,
+      },
     })
-  end
+
+    -- Keymap to toggle layout strategy
+    vim.keymap.set("n", "<leader>tl", function()
+      layout = layout == "vertical" and "horizontal" or "vertical"
+      telescope.setup({
+        defaults = {
+          layout_strategy = layout,
+        },
+      })
+      print("Telescope layout set to: " .. layout)
+    end, { desc = "Toggle Telescope layout" })
+  end,
 }
