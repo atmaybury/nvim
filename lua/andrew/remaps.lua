d79277d0597d47963faebcc18073885973146d12
Andrew Maybury <andrew.maybury@leaveplus.com.au>
2025-11-11 14:58:24 +1100

    Added remap to find uses of function

--- lua/andrew/remaps.lua
+++ lua/andrew/remaps.lua
@@ -1,9 +1,10 @@
--- key remappings
+-- telescope remappings
 local builtin = require("telescope.builtin")
 vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+vim.keymap.set('n', '<leader>fr', builtin.lsp_references, {})
 
 -- close buffer
 vim.keymap.set('n', '<leader>q', ":bd<CR>")
