5e66b94b710835f4131e69099789e47ba9edfeb8
Andrew Maybury <andrew.maybury@leaveplus.com.au>
2025-12-12 13:27:42 +1100

    Increased conform timeout

--- lua/andrew/lazy/conform.lua
+++ lua/andrew/lazy/conform.lua
@@ -24,9 +24,10 @@ return {
       typescriptreact = { "prettier" },
       yaml = { "prettier" },
       json = { "prettier" },
+      graphql = { "prettier" },
     },
     -- Set up format-on-save
-    format_on_save = { timeout_ms = 500, lsp_fallback = true },
+    format_on_save = { timeout_ms = 1000, lsp_fallback = true },
     -- Customize formatters
     formatters = {
       shfmt = {
