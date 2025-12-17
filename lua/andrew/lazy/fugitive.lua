cc919af226c3c02baef18543d71cfa46e097923e
Andrew Maybury <andrew.maybury@leaveplus.com.au>
2025-12-17 15:59:36 +1100

    Added remap for opening Git vert split

--- lua/andrew/lazy/fugitive.lua
+++ lua/andrew/lazy/fugitive.lua
@@ -1,1 +1,6 @@
-return { "tpope/vim-fugitive" }
+return {
+  "tpope/vim-fugitive",
+  keys = {
+    { "<leader>g", function() vim.cmd("vertical Git") end, desc = "Fugitive status (vertical)" },
+  }
+}
