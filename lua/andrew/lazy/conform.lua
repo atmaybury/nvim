55972b51a7e5bc6a4c337a7b3874cf02b9e486fa
Andrew Maybury <andrew.maybury@leaveplus.com.au>
2026-01-06 12:59:02 +1100

    Added formatting for cucumber

--- lua/andrew/lazy/conform.lua
+++ lua/andrew/lazy/conform.lua
@@ -25,6 +25,7 @@ return {
       yaml = { "prettier" },
       json = { "prettier" },
       graphql = { "prettier" },
+      cucumber = { "prettier" }
     },
     -- Set up format-on-save
     format_on_save = { timeout_ms = 1000, lsp_fallback = true },
