28a34dac668c4b0e94bdb14989d60a62f91f38cb
Andrew Maybury <andrew.maybury@leaveplus.com.au>
2026-02-19 09:42:27 +1100

    Removed conflicting bindings

--- lua/andrew/lazy/oil.lua
+++ lua/andrew/lazy/oil.lua
@@ -5,6 +5,12 @@ return {
     default_file_explorer = true,
     view_options = {
       show_hidden = true
+    },
+    keymaps = {
+      ["<C-h>"] = false,
+      ["<C-l>"] = false,
+      ["<C-k>"] = false,
+      ["<C-j>"] = false,
     }
   }
 }
